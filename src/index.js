a9f95cc06ee619b6fedc839112e0647d3b370ca9
Redddeve <popko.dmitry797@gmail.com>
2023-08-22 15:02:33 +0300

    Setting up the project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+function fetch(url) {
+  axios(url);
+}
